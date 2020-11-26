650fbf2417d593c4d4051e5592f3adb37ae37ec1
danielfasanaro <danielfasanaro@gmail.com>
2020-11-26 13:33:46 -0300

    Exercicio 3 - Adicionado script com resolucao

---
+++ exercises/4.2/exercise3.js
@@ -0,0 +1,11 @@
+let number1 = 10;
+let number2 = 20;
+let number3 = 30;
+
+if (number1 > number2 && number1 > number3) {
+  console.log("O maior número é o: " + number1);
+} else if (number2 > number3) {
+  console.log("O maior número é o: " + number2);
+} else {
+  console.log("O maior número é o: " + number3);
+}
