ef0e8a91bf646fb4610f8c6ab21529e890986c30
danielfasanaro <danielfasanaro@gmail.com>
2020-11-26 20:57:45 -0300

    Exercicio 6 - adicionado script com a resolucao

---
+++ exercises/4.3/exercise6.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let oddInNumbers= 0;
+
+for (let indexNumbers = 0 ; indexNumbers < numbers.length ; indexNumbers += 1) {
+  if (numbers[indexNumbers] % 2 != 0) {
+    oddInNumbers += 1;
+  }
+}
+if (oddInNumbers > 0) {
+  console.log('Existem ' + oddInNumbers + ' números ímpares no array.')
+}
