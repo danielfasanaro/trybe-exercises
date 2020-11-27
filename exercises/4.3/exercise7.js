45c88e1121f71c3259ccfd4028beffc9eb6fa440
danielfasanaro <danielfasanaro@gmail.com>
2020-11-26 21:09:50 -0300

    Exercicio 7 - adicionado script com a resolucao

---
+++ exercises/4.3/exercise7.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let minOfNumbers = numbers[0];
+
+for ( let indexNumbers = 1 ; indexNumbers < numbers.length ; indexNumbers += 1) {
+  if (numbers[indexNumbers] < minOfNumbers) {
+    maxOfNumbers = numbers[indexNumbers];
+  }
+}
+console.log('O menor número é: ' + minOfNumbers);
