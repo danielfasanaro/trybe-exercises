4a08fdc693781bcb8c9861cd998b954b9c0c15aa
danielfasanaro <danielfasanaro@gmail.com>
2020-11-27 18:18:23 -0300

    Exercicio 3 - Adicionado script com resolucao

---
+++ exercises/4.4/exercise3.js
@@ -0,0 +1,14 @@
+// Agora inverta o lado do triângulo.
+let n = 5;
+
+for (indexOfN = 0 ; indexOfN < n ; indexOfN += 1) {
+  let asterisks = '';
+  for (indexOfAsterisks = 0 ; indexOfAsterisks <= n ; indexOfAsterisks += 1) {
+    if (indexOfAsterisks >= (n - indexOfN)) {
+      asterisks += '*';
+    }else {
+      asterisks += ' ';
+    }
+  }
+  console.log(asterisks);
+}
