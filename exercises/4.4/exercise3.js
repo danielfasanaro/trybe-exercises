74157e5c0b466ee3ba3962311284dc91e0c2a3a0
danielfasanaro <danielfasanaro@gmail.com>
2020-11-29 00:12:10 -0300

    Exercicio 3 - Corrigido espacamento sobrando

--- exercises/4.4/exercise3.js
+++ exercises/4.4/exercise3.js
@@ -3,7 +3,7 @@ let n = 5;
 
 for (indexOfN = 0 ; indexOfN < n ; indexOfN += 1) {
   let asterisks = '';
-  for (indexOfAsterisks = 0 ; indexOfAsterisks <= n ; indexOfAsterisks += 1) {
+  for (indexOfAsterisks = 1 ; indexOfAsterisks <= n ; indexOfAsterisks += 1) {
     if (indexOfAsterisks >= (n - indexOfN)) {
       asterisks += '*';
     }else {
