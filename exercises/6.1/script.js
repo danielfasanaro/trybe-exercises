49331de1dc51e392d5f9badf4248ebf6c0491122
danielfasanaro <danielfasanaro@gmail.com>
2021-01-05 01:51:48 -0300

    Bloco 6.1 - Exercício 2 - Adicionado segundo fieldset e função para validar data

--- exercises/6.1/script.js
+++ exercises/6.1/script.js
@@ -1,4 +1,5 @@
 const state = document.querySelector('#state');
+const startDate = document.querySelector('#start-date');
 
 const stateList = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
 const stateValues = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
@@ -12,4 +13,20 @@ function addOptions() {
   }
 }
 
+function validateDate(event) {
+  const date = event.target.value.split('/');
+  if (date[0] <= 0 || date[0] > 31) {
+    alert('O dia deve estar no intervalo de 1 a 31!');
+  }
+  if (date[1] <= 0 || date[1] > 12) {
+    alert('O mês deve estar no intervalo de 1 a 12!');
+  }
+  if (date[2] < 0) {
+    alert('O ano não pode ser negativo!');
+  }
+}
+
+
+
 addOptions();
+startDate.addEventListener('change', validateDate);
