eab69d5a385cff0207e50ad52b13eb90fce6c3a5
danielfasanaro <danielfasanaro@gmail.com>
2021-01-05 18:37:31 -0300

    Bloco 6.1 - Exercicios 3 e 4: Adicionados os botoes e implementadas as funcoes para os botoes enviar e limpar formulario.

--- exercises/6.1/script.js
+++ exercises/6.1/script.js
@@ -1,5 +1,11 @@
 const state = document.querySelector('#state');
 const startDate = document.querySelector('#start-date');
+const formOutput = document.querySelector('#form-output');
+const fieldsets = document.querySelectorAll('fieldset');
+const sendFormBtn = document.querySelector('#send-form');
+const cleanFormBtn = document.querySelector('#clean-form');
+const personal = fieldsets[0].children;
+const professional = fieldsets[1].children;
 
 const stateList = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
 const stateValues = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
@@ -8,25 +14,80 @@ function addOptions() {
   for (let index = 0; index < stateList.length; index += 1) {
     const option = document.createElement('option');
     option.innerText = stateList[index];
-    option.value = stateValues[index];
+    // option.value = stateValues[index];
+    option.value = stateList[index];
     state.appendChild(option);
   }
 }
 
-function validateDate(event) {
-  const date = event.target.value.split('/');
-  if (date[0] <= 0 || date[0] > 31) {
-    alert('O dia deve estar no intervalo de 1 a 31!');
+function validateDate() {
+  let validate = true;
+  if (startDate.value) {
+    const date = startDate.value.split('/');
+    if (date[0] <= 0 || date[0] > 31) {
+      alert('O dia deve estar no intervalo de 1 a 31!');
+      validate = false;
+    }
+    if (date[1] <= 0 || date[1] > 12) {
+      alert('O mês deve estar no intervalo de 1 a 12!');
+      validate = false;
+    }
+    if (date[2] < 0) {
+      alert('O ano não pode ser negativo!');
+      validate = false;
+    }
   }
-  if (date[1] <= 0 || date[1] > 12) {
-    alert('O mês deve estar no intervalo de 1 a 12!');
+  return (validate);
+}
+
+function printForm(event) {
+  event.preventDefault();
+  formOutput.innerHTML = '';
+  if (!validateDate()) {
+    return;
   }
-  if (date[2] < 0) {
-    alert('O ano não pode ser negativo!');
+  for (let index = 0; index < personal.length; index += 1) {
+    if (personal[index].value) {
+      if (personal[index].type !== 'radio') {
+        const output = document.createElement('p');
+        output.innerText = `${personal[index -1].innerText} ${personal[index].value}`;
+        formOutput.appendChild(output);
+      }
+      if (personal[index].checked) {
+        const typeLabel = document.querySelector('#type');
+        const output = document.createElement('p');
+        output.innerText = `${typeLabel.innerText} ${personal[index].value}`;
+        formOutput.appendChild(output);
+      }
+    }
+  }
+  for (let index = 0; index < professional.length; index += 1) {
+    if (professional[index].value) {
+      const output = document.createElement('p');
+      output.innerText = `${professional[index -1].innerText} ${professional[index].value}`;
+      formOutput.appendChild(output);
+    }
   }
 }
 
-
+function cleanForm() {
+  formOutput.innerHTML = '';
+  for (let index = 0; index < personal.length; index += 1) {
+    if (personal[index].tagName === 'INPUT') {
+      if (personal[index].type !== 'radio') {
+        personal[index].value = '';
+      } else {
+        personal[index].checked = false;
+      }
+    }
+  }
+  for (let index = 0; index < professional.length; index += 1) {
+    if (professional[index].value) {
+      professional[index].value = '';
+    }
+  }
+}
 
 addOptions();
-startDate.addEventListener('change', validateDate);
+sendFormBtn.addEventListener('click', printForm);
+cleanFormBtn.addEventListener('click', cleanForm);
