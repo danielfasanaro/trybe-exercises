d399ee6846e73f081ba0e842ad386d770375f6da
danielfasanaro <danielfasanaro@gmail.com>
2021-01-05 00:51:57 -0300

    Bloco 6.1 - Exercício 1 - Adicionado primeiro Fieldset

---
+++ exercises/6.1/script.js
@@ -0,0 +1,15 @@
+const state = document.querySelector('#state');
+
+const stateList = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
+const stateValues = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+
+function addOptions() {
+  for (let index = 0; index < stateList.length; index += 1) {
+    const option = document.createElement('option');
+    option.innerText = stateList[index];
+    option.value = stateValues[index];
+    state.appendChild(option);
+  }
+}
+
+addOptions();
