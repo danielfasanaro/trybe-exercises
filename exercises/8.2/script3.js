aacd31a7611626f997f6e35e35ce0b0001d31b55
danielfasanaro <danielfasanaro@gmail.com>
2021-01-19 21:46:08 -0300

    Fixed a semicolon

--- exercises/8.2/script3.js
+++ exercises/8.2/script3.js
@@ -108,7 +108,7 @@ const expectedResult = [
 
 function fantasyOrScienceFiction() {
   // escreva seu código aqui
-  return books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica')
+  return books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
 }
 
 assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
